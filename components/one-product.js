cc072094d1de2819dc940cb897f4d6807291dce6
unknown <enkhmandakh.official@gmail.com>
2023-11-29 09:35:17 +0800

    component taldaa

--- components/one-product.js
+++ components/one-product.js
@@ -1,49 +1,79 @@
 class OneProduct extends HTMLElement {
     constructor() {
         super();
+        this.quantity = 0;
+
         this.innerHTML = `
-        <article>
-            <img src="${this.getAttribute("img")}" alt="ice cream" >
-            <h6>${this.getAttribute("title")}</h6>
-            <p>${this.getAttribute("price")}₮</p>
-            <div class="counts_of_fd">
-                <button id="decrement">-</button>
-                <span>0</span>
-                <button id="increment">+</button>
-            </div>
-        </article>
+            <article>
+                <img src="${this.getAttribute("img")}" alt="ice cream">
+                <h6>${this.getAttribute("title")}</h6>
+                <p>${this.getAttribute("price")}₮</p>
+                <div class="counts_of_fd">
+                    <button id="decrement">-</button>
+                    <span>${this.quantity}</span>
+                    <button id="increment">+</button>
+                </div>
+            </article>
         `;
-
-        // Store a reference to the button during construction
-        this.addButtonListener();
-    }
-
-    addButtonListener() {
-        const addButton = this.querySelector('#increment');
-        addButton.addEventListener("click", () => {
-            const event = new CustomEvent("addToCart", {
-                detail: {
-                    title: this.getAttribute("title"),
-                    price: this.getAttribute("price")
-                }
-            });
-            document.dispatchEvent(event);
-            console.log("aaaa");
-        });
     }
 
     connectedCallback() {
-        // Reattach event listener in case the element was disconnected and reconnected
-        this.addButtonListener();
+        // Add event listeners here, in the connectedCallback
+        const incrementButton = this.querySelector('#increment');
+        const decrementButton = this.querySelector('#decrement');
+
+        incrementButton.addEventListener("click", () => {
+            this.quantity += 1;
+            this.updateQuantity();
+            this.dispatchEventToCart();
+        });
+
+        decrementButton.addEventListener("click", () => {
+            if (this.quantity > 0) {
+                this.quantity -= 1;
+                this.updateQuantity();
+                this.dispatchEventToCart();
+            }
+        });
+    }
+
+    updateQuantity() {
+        const quantityElement = this.querySelector('span');
+        quantityElement.textContent = this.quantity;
+    }
+
+    dispatchEventToCart() {
+        const event = new CustomEvent("addToCart", {
+            detail: {
+                title: this.getAttribute("title"),
+                price: this.getAttribute("price"),
+                quantity: this.quantity,
+            },
+        });
+        document.dispatchEvent(event);
     }
 
     disconnectedCallback() {
-        // Cleanup tasks can be added here if needed
+        // Remove event listeners here, in the disconnectedCallback if needed
+        const incrementButton = this.querySelector('#increment');
+        const decrementButton = this.querySelector('#decrement');
+
+        incrementButton.removeEventListener("click", () => {
+            this.quantity += 1;
+            this.updateQuantity();
+            this.dispatchEventToCart();
+        });
+
+        decrementButton.removeEventListener("click", () => {
+            if (this.quantity > 0) {
+                this.quantity -= 1;
+                this.updateQuantity();
+                this.dispatchEventToCart();
+            }
+        });
     }
 
-    attributeChangedCallback() {
-        // Handle attribute changes if needed
-    }
+    attributeChangedCallback() {}
 }
 
 window.customElements.define("one-product", OneProduct);
