4ba7cf94a5357f8749d6d87c6b49360d776a9e28
hngrzl0 <124540636+hngrzl0@users.noreply.github.com>
2023-12-13 08:09:44 +0800

    shadow dom

--- components/ticket-type.js
+++ components/ticket-type.js
@@ -5,7 +5,7 @@ class TicketType extends HTMLElement {
         this.cost = 0;
         this.type = this.getAttribute('type') || 'Том Хүн';
         this.costPerTicket = parseFloat(this.getAttribute('cost')) || 15000;
-        this.attachShadow({ mode: 'open' });
+        this.attachShadow({ mode: 'closed' });
         this.shadowRoot.innerHTML = `
         <style>
             li{
