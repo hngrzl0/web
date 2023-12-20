d2ce4cba82414ccea7c2fcce99c1f3c23b553f16
hngrzl0 <124540636+hngrzl0@users.noreply.github.com>
2023-12-20 09:42:24 +0800

    session_ram

---
+++ session_ram/app.mjs
@@ -0,0 +1,43 @@
+
+import path from 'path';
+import express from 'express';
+//import cookieParser from 'cookie-parser';
+import { login } from './login.mjs'
+import Login from './login.mjs';
+
+
+const app = express()
+const port = 3000
+const __dirname = path.resolve(path.dirname(''));
+const options = {
+    root: path.join(__dirname)
+};
+//app.use(cookieParser());
+app.use(express.json());
+
+login.users.set('zul', { fullname: "Zul", password: "123" });
+login.users.set('admin', { fullname: "Admin", password: "123" });
+
+
+app.get('/', (req, res) => {
+    res.sendFile("./login.html", options);
+})
+
+app.post('/login', login.verifyLogin.bind(login));
+
+app.get('/logout', (req, res) => { 
+    user.sessions.delete(Number(req.cookies.session_id));
+    res.status(200).send();
+});
+
+app.get('/emails', (req, res) => {
+    if (!login.sessions.has(Number(req.cookies.session_id)))
+    {
+        res.sendFile('./forbidden.html', options);
+        return;
+    }
+    
+    res.send(`<html><h1>Inbox for ${login.sessions.get(Number(req.cookies.session_id)).fullname} </h1><ul><li>Email1</li><li>Email2</li></ul></html>`);
+})
+
+app.listen(port, () => console.log(`Example app listening on port http://localhost:${port}`))
---
+++ session_ram/login.mjs
@@ -0,0 +1,37 @@
+export default class Login {
+    constructor() {
+        this.sessions = new Map();
+        this.users = new Map();
+    }
+
+    verifyLogin(req, res) {
+        const email = req.body.email, pass = req.body.password;
+        console.log(email, pass);
+
+        if (!this.users.has(email) || this.users.get(email).password != pass) {
+            res.status(403).end();
+            return;
+        }
+
+        const sid = Math.floor(Math.random() * 100_000_000_000_000);
+        this.sessions.set(sid,
+            {
+                user: email,
+                fullname: this.users.get(email).fullname,
+                logged: Date.now()
+            });
+        console.log(this.sessions);
+
+        res.status(200);
+        res.cookie("session_id", sid);
+        res.send({
+            result: "OK",
+            username: this.users.get(email).fullname
+        });
+
+    }
+}
+
+export const login = new Login();
+
+
