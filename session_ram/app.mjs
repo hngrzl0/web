c3820bdeba92228440bd54beea1ac3d3127c030c
hngrzl0 <124540636+hngrzl0@users.noreply.github.com>
2024-01-02 13:52:13 +0800

    swagger tsegtslev

--- session_ram/app.mjs
+++ session_ram/app.mjs
@@ -3,31 +3,67 @@ import express from 'express';
 import { login } from './login.mjs'
 import { register } from './register.mjs';
 import { getusers } from './users.mjs';
-import { setupSwagger } from './swagger.js';
+import swaggerUi from "swagger-ui-express";
+import swaggerJsondoc from "swagger-jsdoc";
 const app = express()
 const port = 3000
 const __dirname = path.resolve(path.dirname(''));
-const options = {
-    root: path.join(__dirname)
-};
 //json data huleej avdg bolno
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
-setupSwagger(app);
+const options = {
+    root: path.join(__dirname),
+    swaggerDefinition: {
+        openapi: "3.0.0",
+        info: {
+            title: "TEK API",
+            version: "1.0.0",
+            description:
+                "API for TEK community", 
+            license: {
+                name: "NUM",
+                url: "https://num.edu.mn/"
+            },
+            contact: {
+                name: "WebDevAdmin",
+                url: "https://www.num.edu.mn/",
+                email: "21B1NUM0165@num.edu.mn"
+            }
+        },
+        servers: [
+            {
+                url: "http://localhost:3000/"
+            }
+        ]
+    },
+    apis: ["session_ram/app.mjs"]
+};
+const specs = swaggerJsondoc(options);
+app.use("/docs", swaggerUi.serve);
+app.get(
+    "/docs",
+    swaggerUi.setup(specs, {
+        explorer: true
+    })
+);
 /**
  * @swagger
  * tags:
  *  -
  *   name: "Authentication"
- *   description: User authentication operations
+ *   description: Хэрэглэгч баталгаажуулалт
  *      
  *  - 
  *   name: "Registration"
- *   description: User registration operations 
+ *   description: Хэрэглэгч бүртгүүлэлт 
  *
  *  - 
  *   name: "Users"
- *   description: User-related operations 
+ *   description: Хэрэглэгчийн мэдээлэлтэй холбоотой 
+ *  
+ *  - 
+ *   name: "Static Files"
+ *   description: Файлтай холбоотой  
  */
 /**
  * @swagger
@@ -35,11 +71,11 @@ setupSwagger(app);
  *  /:
  *    get:
  *      tags: 
- *        - Authentication
- *      summary: Serve login page
+ *        - Static Files
+ *      summary: Нэвтрэх хуудас
  *      responses:
  *        "200":
- *          description: Login page served successfully
+ *          description: Нэвтрэх хуудас амжилттай дамжуулагдав.
  *          content:
  *            text/html:
  *              schema:
@@ -56,24 +92,24 @@ app.get('/', (req, res) => {
  *    post:
  *      tags:
  *        - Authentication
- *      summary: Authenticate user
+ *      summary: Хэрэгдэгчийг баталгаажуулах
  *      requestBody:
- *        description: User credentials
+ *        description: Хэрэглэгчийн мэдээлэл шаардлагатай
  *        required: true
  *        content:
  *          application/json:
  *            schema:
  *              type: object
  *              properties:
- *                username:
+ *                email:
  *                  type: string
  *                password:
  *                  type: string
  *      responses:
  *        "200":
- *          description: Successful authentication
- *        "401":
- *          description: Unauthorized
+ *          description: Нэвтрэлт амжилттай.
+ *        "403":
+ *          description: Нэвтрэх мэдээлэл хангалтгүй.
  */
 app.post('/login', login.verifyLogin.bind(login));
 
@@ -83,11 +119,11 @@ app.post('/login', login.verifyLogin.bind(login));
  *  /register:
  *    get:
  *      tags:
- *        - Registration
- *      summary: Serve registration page
+ *        - Static Files
+ *      summary: Бүртгүүлэх хуудас
  *      responses:
  *        "200":
- *          description: Registration page served successfully
+ *          description: Бүртгүүлэх хуудас амжилттай дамжуулагдав.
  *          content:
  *            text/html:
  *              schema:
@@ -104,26 +140,28 @@ app.get('/register', (req, res) => {
  *    post:
  *      tags:
  *        - Registration
- *      summary: Register a new user
+ *      summary: Шинэ хэрэглэгч бүртгэх
  *      requestBody:
- *        description: User registration details
+ *        description: хэрэглэгчийн мэдээлэл шаардлагатай.
  *        required: true
  *        content:
  *          application/json:
  *            schema:
  *              type: object
  *              properties:
- *                username:
+ *                email:
+ *                  type: string
+ *                fullname:
  *                  type: string
  *                password:
  *                  type: string
- *                email:
- *                  type: string
  *      responses:
- *        "201":
- *          description: User successfully registered
+ *        "200":
+ *          description: Хэрэглэгчийг амжилттай бүртгэв.
  *        "400":
- *          description: Bad request
+ *          description: Хэрэглэгчийн мэдээлэл хангалтгүй.
+ *        "500":
+ *          description: Бүртгэл амжилтгүй.
  */
 app.post('/registeruser', register.verifyRegister.bind(register));
 
@@ -134,10 +172,12 @@ app.post('/registeruser', register.verifyRegister.bind(register));
  *    get:
  *      tags:
  *        - Users
- *      summary: Get user data
+ *      summary: Хэрэглэгчийн мэдээлэл авах
  *      responses:
  *        "200":
- *          description: User data retrieved successfully
+ *          description: Хэрэглэгчийн мэдээлэл амжилттай дамжуулагдав.
+ *        "404":
+ *          description: Хэрэглэгчийн мэдээлэл олдсонгүй.
  *          content:
  *            application/json:
  *              schema:
@@ -147,24 +187,105 @@ app.post('/registeruser', register.verifyRegister.bind(register));
  *                  properties:
  *                    id:
  *                      type: integer
- *                    username:
+ *                    loginname:
  *                      type: string
- *                    email:
+ *                    fullname:
  *                      type: string
  */
 app.get('/users', getusers.get.bind(getusers));
+/**
+ * @swagger
+ * paths:
+ *  /index.html:
+ *    get:
+ *      tags:
+ *        - Static Files
+ *      summary: Нүүр хуудас
+ *      responses:
+ *        "200":
+ *          description: Нүүр хуудас амжилттай дамжуулагдав.
+ *          content:
+ *            text/html:
+ *              schema:
+ *                type: string
+ */
 app.get('/index.html', (req, res) => {
     res.sendFile("index.html", options);
 });
+/**
+ * @swagger
+ * paths:
+ *  /js_now.html:
+ *    get:
+ *      tags:
+ *        - Static Files
+ *      summary: Кино хуудас
+ *      responses:
+ *        "200":
+ *          description: Кино хуудас амжилттай дамжуулагдав.
+ *          content:
+ *            text/html:
+ *              schema:
+ *                type: string
+ */
 app.get('/js_now.html', (req, res) => {
     res.sendFile("js_now.html", options);
 });
+
+/**
+ * @swagger
+ * paths:
+ *  /movie.html:
+ *    get:
+ *      tags:
+ *        - Static Files
+ *      summary: Кино2 хуудас
+ *      responses:
+ *        "200":
+ *          description: Кино хуудас амжилттай дамжуулагдав.
+ *          content:
+ *            text/html:
+ *              schema:
+ *                type: string
+ */
 app.get('/movie.html', (req, res) => {
     res.sendFile("movie.html", options);
 });
+/**
+ * @swagger
+ * paths:
+ *  /ticketing.html:
+ *    get:
+ *      tags:
+ *        - Static Files
+ *      summary: Захиалга хуудас
+ *      responses:
+ *        "200":
+ *          description: Захиалга хуудас амжилттай дамжуулагдав.
+ *          content:
+ *            text/html:
+ *              schema:
+ *                type: string
+ */
 app.get('/ticketing.html', (req, res) => {
     res.sendFile("ticketing.html", options);
 });
+/**
+ * @swagger
+ * paths:
+ *  /login.html:
+ *    get:
+ *      tags:
+ *        - Static Files
+ *      summary: Нэвтрэх хуудас
+ *      responses:
+ *        "200":
+ *          description: Нэвтрэх хуудас амжилттай дамжуулагдав.
+ *          content:
+ *            text/html:
+ *              schema:
+ *                type: string
+ */
 app.get('/login.html', (req, res) => {
     res.sendFile("login.html", options);
 })
