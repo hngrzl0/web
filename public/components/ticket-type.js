e23af5f20e8d59a63009df689a34c9f571b6b909
hngrzl0 <124540636+hngrzl0@users.noreply.github.com>
2024-01-02 20:06:49 +0800

    tsegtslev

--- public/components/ticket-type.js
+++ public/components/ticket-type.js
@@ -1,10 +1,13 @@
 class TicketType extends HTMLElement {
     constructor() {
-        super();
+        super();   //superclass iin constructor function duudagdna.
+        //hergtei ugugdluude todorhoilno.
         this.quantity = 0;
         this.cost = 0;
+        //attribute damjuulagdagu bvl default utgaar type aa tom hun cost oo 15k gj avna.
         this.type = this.getAttribute('type') || 'Том Хүн';
         this.costPerTicket = parseFloat(this.getAttribute('cost')) || 15000;
+        //nuutslald zoriulj shadow DOM ashiglaj bolno. mode oo closed bolgovl inspect dr haragdahgui.
         this.attachShadow({ mode: 'open' });
         this.shadowRoot.innerHTML = `
         <style>
@@ -48,50 +51,51 @@ class TicketType extends HTMLElement {
             </li>
         `;
     }
-
+    //element DOM-d holbogdh uyd duudagdh method
     connectedCallback() {
+        //nemeh hasah button avah
         const incrementButton = this.shadowRoot.getElementById('incrementBtn');
         const decrementButton = this.shadowRoot.getElementById('decrementBtn');
-
+        //nemeh hasah buttond click event listener hiih
         incrementButton.addEventListener('click', () => {
+            //nemeh buttond click event yvgdhd toog 1r nemj, delgetsed shinechlelt hiij, addToCart eventd ilgeene.
             this.quantity += 1;
             this.updateQuantityAndCost();
             this.dispatchEventToCart();
         });
-
         decrementButton.addEventListener('click', () => {
+            //hasah buttond click event yvgdhd toog 1r hasj, delgetsed shinechlelt hiij, addToCart eventd ilgeene.
             if (this.quantity > 0) {
                 this.quantity -= 1;
                 this.updateQuantityAndCost();
                 this.dispatchEventToCart();
             }
         });
-
+        //une too hemjeegee shinechleh
         this.updateQuantityAndCost(); 
     }
-
+    //une too hemjeegee shinechleh method
     updateQuantityAndCost() {
-        this.updateQuantity();
-        this.updateCost();
-
-        this.dispatchEventToCart();
+        this.updateQuantity(); //too hemjee shinechleh
+        this.updateCost();//une shinechleh
+        this.dispatchEventToCart();//cartd medegdh
     }
-
+    //too hemjeeg shinechleh method
     updateQuantity() {
         const quantityElement = this.shadowRoot.querySelector('span');
         quantityElement.textContent = this.quantity;
     }
-
+    //uneig bodoj shinechleh method
     updateCost() {
         const costElement = this.shadowRoot.querySelector('.cost');
         costElement.textContent = `${this.calculateCost()}₮`;
     }
-
+    //ticketnii uneiig odoogin too hemjeeger urjuulj bodoh method
     calculateCost() {
         this.cost = this.quantity * this.costPerTicket;
         return this.cost;
     }
-
+    ////addToCart custom elementiig ticketnii type cost iin hamt dispatch hiih method
     dispatchEventToCart() {
         const event = new CustomEvent("addToCart", {
             detail: {
@@ -100,19 +104,18 @@ class TicketType extends HTMLElement {
                 quantity: this.quantity,
             },
         });
-
-        document.dispatchEvent(event);
+        document.dispatchEvent(event);//addToCart eventiig busd hesegt notify hiih
     }
-
+    //element DOM-s removelgdhd duudagdh method
     disconnectedCallback() {
+        // nemeh hasah buttonoo avah    
         const incrementButton = this.shadowRoot.getElementById('incrementBtn');
         const decrementButton = this.shadowRoot.getElementById('decrementBtn');
-
+        //eventlistenet uude ustgah 
         incrementButton.removeEventListener('click', () => {
             this.quantity += 1;
             this.updateQuantityAndCost();
         });
-
         decrementButton.removeEventListener('click', () => {
             if (this.quantity > 0) {
                 this.quantity -= 1;
